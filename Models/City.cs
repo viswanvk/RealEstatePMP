a86a035fc29b7dea22235f15bdf421659e0efc53
Prakhar015 <89700399+Prakhar015@users.noreply.github.com>
2021-10-02 21:34:17 -0400

    Adding Validations

--- Models/City.cs
+++ Models/City.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace RealEstatePMP.Models
 {
@@ -9,7 +11,15 @@ namespace RealEstatePMP.Models
     {
 
         public int CityId { get; set; }
+
+        [DisplayName("City")]
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
+
+        [DisplayName("State")]
+        [Required]
+        [StringLength(50)]
         public string State { get; set; }
         public List<Dwelling> Dwellings { get; set; }
 
